8a706025e1a2f2f30a187acc6bc63840f310cf01
Marco <marcoaluchi@hotmail.com>
2020-12-22 18:43:57 -0300

    se estilaron los elementos y se agrego filtrado al clickear

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,37 @@
 import React, { useContext, Fragment } from "react";
 import styled from "@emotion/styled";
+import { nanoid } from "nanoid";
 import { TodoContext } from "./../context/TodoContext";
+
+const ItemTarea = styled.div`
+  display: flex;
+`;
+const ItemTareaTexto = styled.h4`
+  margin-right: 1rem;
+`;
+const Boton = styled.button`
+  margin: 0.5rem;
+  align-self: center;
+  height: 1.8rem;
+`;
 const TaskList = () => {
   const { listaTareas, setListaTareas } = useContext(TodoContext);
+  const handleClick = (id) => {
+    setListaTareas(listaTareas.filter((tarea) => id !== tarea.id));
+  };
   return (
     <Fragment>
       <h1>Lista De Tareas</h1>
       {listaTareas.map((tarea) => (
-        <p>{tarea}</p>
+        <ItemTarea key={nanoid()}>
+          <ItemTareaTexto>{tarea.tarea}</ItemTareaTexto>
+          <Boton onClick={() => handleClick(tarea.id)}>
+            <img
+              src="https://icongr.am/fontawesome/close.svg?size=23&color=currentColor"
+              alt="Delete"
+            />
+          </Boton>
+        </ItemTarea>
       ))}
     </Fragment>
   );
