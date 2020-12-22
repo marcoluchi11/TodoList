f8c4325acde4a3bc48025a599cddfccd43b1807e
Marco <marcoaluchi@hotmail.com>
2020-12-22 18:44:22 -0300

    se agrego validacion y se muestra error

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import styled from "@emotion/styled";
+import Error from "./Error";
+import { nanoid } from "nanoid";
 import { TodoContext } from "./../context/TodoContext";
 const BotonSubmit = styled.input`
   background-color: #008cba;
-  font-size: 1.1rem;
+  font-size: 1.3rem;
   padding: 1rem;
   color: #ffffff;
   outline: none;
@@ -11,19 +13,27 @@ const BotonSubmit = styled.input`
   border-radius: 6px;
   cursor: pointer;
   margin-left: 1rem;
+  margin-bottom: 3rem;
+  margin-top: 0.3rem;
+  width: 100%;
 `;
 const Input = styled.input`
   background-color: transparent;
   border-radius: 24px;
   border: 0.5px solid black;
-  height: 2rem;
+  height: 2.9rem;
   margin: 1rem;
   padding: 1rem;
 `;
 const AddTask = () => {
-  const { tareaNueva, setTareaNueva, listaTareas, setListaTareas } = useContext(
-    TodoContext
-  );
+  const {
+    tareaNueva,
+    setTareaNueva,
+    listaTareas,
+    setListaTareas,
+    error,
+    setError,
+  } = useContext(TodoContext);
 
   const { tarea } = tareaNueva;
   const handleChange = (e) => {
@@ -31,25 +41,36 @@ const AddTask = () => {
   };
   const handleClick = (e) => {
     e.preventDefault();
-    setListaTareas([...listaTareas, tarea]);
+    if (tarea.trim() === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    setListaTareas([...listaTareas, { id: nanoid(), tarea: tarea }]);
     setTareaNueva({ tarea: "" });
   };
   return (
-    <form>
-      <Input
-        onChange={handleChange}
-        type="text"
-        name="tarea"
-        placeholder="Añade una tarea..."
-        value={tarea}
-      />
-      <BotonSubmit
-        onClick={handleClick}
-        className="addTask"
-        type="submit"
-        value=" + Añadir Tarea"
-      />
-    </form>
+    <Fragment>
+      <form>
+        <Input
+          className="inputTexto"
+          onChange={handleChange}
+          type="text"
+          name="tarea"
+          placeholder="Añade una tarea..."
+          value={tarea}
+        />
+
+        <BotonSubmit
+          onClick={handleClick}
+          className="addTask"
+          type="submit"
+          value=" + Añadir Tarea"
+        />
+      </form>
+      {error ? <Error /> : null}
+    </Fragment>
   );
 };
 
