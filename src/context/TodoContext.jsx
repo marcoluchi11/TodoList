dfbcffbba20b5f8cf1fccd2a9f1e0d2af2fc33c6
Marco <marcoaluchi@hotmail.com>
2020-12-26 16:52:02 -0300

    se agrego func de localStorage

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -4,7 +4,10 @@ export const TodoContext = createContext();
 
 const TodoProvider = (props) => {
   const [tareaNueva, setTareaNueva] = useState({ tarea: "" });
-  const [listaTareas, setListaTareas] = useState([]);
+  const [listaTareas, setListaTareas] = useState(() => {
+    const listovich = localStorage.getItem("listatareas");
+    return listovich ? JSON.parse(listovich) : [];
+  });
   const [error, setError] = useState(false);
   return (
     <TodoContext.Provider
