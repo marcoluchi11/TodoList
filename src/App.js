df7879e7e029dddef650ef036a24155e31a740d1
Marco <marcoaluchi@hotmail.com>
2020-12-30 15:34:58 -0300

    maquetado del Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
+import Login from "./components/Login";
 import TodoProvider from "./context/TodoContext";
 function App() {
   return (
     <TodoProvider>
       <div className="contenedor">
+        <div className="login">
+          <Login />
+        </div>
         <div className="addTarea">
           <AddTask />
         </div>
