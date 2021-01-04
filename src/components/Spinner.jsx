4184cf224d6f87b722ed779d9a56914ae07b4e7c
Marco <marcoaluchi@hotmail.com>
2021-01-04 17:03:44 -0300

    se agrego spinner para esperar a la llegada de las tareas de la database

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import "./Spinner.css";
+import styled from "@emotion/styled";
+const Container = styled.div`
+  display: flex;
+  padding: 0.5rem;
+  p {
+    margin-right: 0.5rem;
+  }
+`;
+const Spinner = () => {
+  return (
+    <Container>
+      <p> Cargando Tareas...</p>
+      <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default Spinner;
