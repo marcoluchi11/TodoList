abeba81e161c7bd98df0c955b3e759516dda431e
Marco <marcoaluchi@hotmail.com>
2021-01-07 16:18:03 -0300

    cambios en el disenio

--- src/components/LoginCompletado.jsx
+++ src/components/LoginCompletado.jsx
@@ -2,9 +2,14 @@ import React, { Fragment } from "react";
 import styled from "@emotion/styled";
 
 const BotonSalir = styled.input`
-  padding: 0 1.3rem;
-  margin: 1rem;
   font-size: 0.9rem;
+  margin: 1rem;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 7px;
+
+  background-color: #14274e;
+  color: #f1f6f9;
   cursor: pointer;
 `;
 const LoginCompletado = ({ email, handleLogout, setLogin }) => {
